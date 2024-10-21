7ea1a37a2b30d106233627e7a59ce9cb62017d9a
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2024-10-21 10:47:27 +0530

    Sorting0s1s2s

--- src/arrayProblems/ArraySorting0s1s2s.java
+++ src/arrayProblems/ArraySorting0s1s2s.java
@@ -34,6 +34,35 @@ public class ArraySorting0s1s2s {
 //		System.out.println(Arrays.toString(arr));
 		
 		//2nd Way
+//		int c0=0, c1=0,c2=0;
+//		for(int i=0;i<len;i++) {
+//			if(arr[i]==0) {
+//				c0++;
+//			}else if(arr[i]==1) {
+//				c1++;
+//			}else {
+//				c2++;
+//			}
+//		}
+//
+//		int id=0;
+//		
+//		for(int i=0;i<len;i++) {
+//			if(c0!=0) {
+//				arr[id++]=0;
+//				c0--;
+//			}else if(c1!=0) {
+//				arr[id++]=1;
+//				c1--;
+//			}else if(c2!=0) {
+//				arr[id++]=2;
+//				c2--;
+//			}
+//		}
+//		
+//		System.out.println(Arrays.toString(arr));
+		
+		//3rd way
 		
 		
 		
