73e3b3f7b2e3aa83ab0bef67637d7490a1972b52
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2024-10-23 23:28:12 +0530

    MinMaxElementInArray

---
+++ src/arrayProblemsset2/MaximumMinimumElementInArray.java
@@ -0,0 +1,23 @@
+package arrayProblemsset2;
+
+public class MaximumMinimumElementInArray {
+
+
+	public static void main(String[] args) {
+		int arr[] = {34,3,464,545,8};
+		int len =arr.length;
+		int max = arr[0];
+		int min =arr[0];
+		for(int i=0;i<len;i++) {
+			if(arr[i]>max) {
+				max = arr[i];
+			}else if(arr[i]<min) {
+				min = arr[i];
+			}
+		}
+		System.out.println(max);
+		System.out.println(min);
+	}
+
+}
+
