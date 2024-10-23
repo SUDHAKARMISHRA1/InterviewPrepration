98da2782f4fd25d8a3a513d11ee15c61414bb0a0
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2024-10-23 10:37:51 +0530

    Arrayreverse

---
+++ src/arrayProblemsset2/ArrayReverse.java
@@ -0,0 +1,22 @@
+package arrayProblemsset2;
+
+import java.util.Arrays;
+
+public class ArrayReverse {
+
+	public static void main(String[] args) {
+		int arr[] = {5,3,2,4,1};
+		int len = arr.length;
+		int temp=0;
+		for(int i=0;i<len;i++) {
+			for(int j=i+1;j<len;j++) {
+				if(arr[i]>arr[j]) {
+					temp = arr[i];
+					arr[i]= arr[j];
+					arr[j]= temp;
+				}
+			}
+		}System.out.println(Arrays.toString(arr));
+		
+	}
+}
