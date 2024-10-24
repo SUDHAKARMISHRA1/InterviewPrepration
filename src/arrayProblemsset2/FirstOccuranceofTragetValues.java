80e0b7485298b0c9200f5b4209ac2419d0687d60
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2024-10-24 10:18:12 +0530

    FirstOccuranceofTargetValue

---
+++ src/arrayProblemsset2/FirstOccuranceofTragetValues.java
@@ -0,0 +1,16 @@
+package arrayProblemsset2;
+
+public class FirstOccuranceofTragetValues {
+
+	public static void main(String[] args) {
+		int arr[] = {1,2,5,3,4,5,2,3,45};
+		int target =5;
+		int len = arr.length;
+		for(int i=0;i<len;i++) {
+			if(arr[i]==target) {
+				System.out.println("The First Occurance of Target value is at Index " +i);
+				break;
+			}
+		}
+	}
+}
