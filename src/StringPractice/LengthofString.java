fd1a7e3da37841dcafcd8f96149c8c488536a4a3
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2024-10-31 11:42:38 +0530

    Stringlength

---
+++ src/StringPractice/LengthofString.java
@@ -0,0 +1,10 @@
+package StringPractice;
+
+public class LengthofString {
+
+	public static void main(String[] args) {
+		String name = "sudhakar";
+		int len = name.length();
+		System.out.println(len);
+	}
+}
