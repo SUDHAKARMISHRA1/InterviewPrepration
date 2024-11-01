7a0263472cec9b8aae65d7181d4e8aebf32babff
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2024-11-01 23:03:19 +0530

    RemoveduplicateChar

--- src/StringPractice/RemoveDuplicateCharacterforString.java
+++ src/StringPractice/RemoveDuplicateCharacterforString.java
@@ -6,21 +6,31 @@ import java.util.Set;
 public class RemoveDuplicateCharacterforString {
 	
 	public static void main(String[] args) {
-		String name = "sudhakarar";
+		String name = "aasudhakarar";
 		int len = name.length();
 		
 		
-		
-		
-		
+		//2nd way
+//		String clean = "";
+//		for(int i=0;i<len;i++) {
+//			for(int j=i+1;j<len;j++) {
+//				if(name.charAt(i)!=name.charAt(j)) {
+//					if(clean.contains(Character.toString(name.charAt(i)))==false) {
+//						clean = clean + name.charAt(i);
+//					}
+//				}
+//			}
+//		}
+//		
+//		System.out.println(clean);
 		
 		
 		//1st way
-		Set<Character> s = new HashSet<Character>();
-		for(int i=0;i<len;i++) {
-			s.add(name.charAt(i));
-		}
-		System.out.println(s);
+//		Set<Character> s = new HashSet<Character>();
+//		for(int i=0;i<len;i++) {
+//			s.add(name.charAt(i));
+//		}
+//		System.out.println(s);
 			
 	}
 
