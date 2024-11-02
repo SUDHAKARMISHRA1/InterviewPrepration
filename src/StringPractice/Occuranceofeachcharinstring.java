327ad5efb9da523a73cfe454a63b7092e8984b29
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2024-11-02 11:46:26 +0530

    occuranceofcharinstring

--- src/StringPractice/Occuranceofeachcharinstring.java
+++ src/StringPractice/Occuranceofeachcharinstring.java
@@ -9,16 +9,24 @@ public class Occuranceofeachcharinstring {
 		int len = name.length();
 
 		HashMap<Character, Integer> a = new HashMap<Character, Integer>();
-		for(int i=len-1;i>=0;i--) {
+		for(int i=0;i<len;i++) {
 			if(a.containsKey(name.charAt(i))) {
 				int count = a.get(name.charAt(i));
 				a.put(name.charAt(i), ++count);
 			}else {
 				a.put(name.charAt(i), 1);
-
 			}
-
-		}
+		}		
+		//		for(int i=len-1;i>=0;i--) {
+//			if(a.containsKey(name.charAt(i))) {	
+//				int count = a.get(name.charAt(i));
+//				a.put(name.charAt(i), ++count);
+//			}else {
+//				a.put(name.charAt(i), 1);
+//
+//			}
+//
+//		}
 
 		System.out.println(a);
 
