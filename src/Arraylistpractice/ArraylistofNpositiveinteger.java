901c292f99b5371e5f178ca9293b9278cfdadc16
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2024-11-07 22:48:46 +0530

    Listofnpositiveinteger

---
+++ src/Arraylistpractice/ArraylistofNpositiveinteger.java
@@ -0,0 +1,16 @@
+package Arraylistpractice;
+
+import java.util.ArrayList;
+
+public class ArraylistofNpositiveinteger {
+
+	public static void main(String[] args) {
+		int n =20;
+		ArrayList<Integer> ar = new ArrayList<Integer>();
+		for(int i=0;i<n;i++) {
+			ar.add(i);
+		}
+		System.out.println(ar);
+		
+	}
+}
