c795a9902ff15b105260979ebd4b861a48a031ba
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2024-11-13 22:35:53 +0530

    valueofgivenkey

---
+++ src/HashmapPractice/Valuesofgivenkey.java
@@ -0,0 +1,25 @@
+package HashmapPractice;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class Valuesofgivenkey {
+
+	public static void main(String[] args) {
+		HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+		hm.put(1, 3);
+		hm.put(2, 4);
+		int key =4;
+		int len = hm.size();
+		Iterator<Integer> it = hm.keySet().iterator();
+		while(it.hasNext()) {
+			Integer k = it.next();
+			if(k==key) {
+				int val = hm.get(k);
+				System.out.println(val);
+			}
+		}
+		
+	
+	}
+}
