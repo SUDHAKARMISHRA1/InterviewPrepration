cc960a6f23e3b25d3178a6094579c298ee7c60ed
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2024-11-13 10:37:35 +0530

    iterationonmapusingiterator

---
+++ src/HashmapPractice/IterationusingIterator.java
@@ -0,0 +1,29 @@
+package HashmapPractice;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class IterationusingIterator {
+	
+	public static void main(String[] args) {
+		HashMap<String, String> hm = new HashMap<String, String>();
+		hm.put("name", "Sudhakar");
+		hm.put("age", "25");
+		hm.put("nam", "Sudakar");
+		hm.put("ag", "5");
+		int len = hm.size();
+		Iterator<Map.Entry<String, String>> it = hm.entrySet().iterator();
+		while(it.hasNext()) {
+			Map.Entry<String, String> entry = it.next();
+			String key = entry.getKey();
+			String value = entry.getValue();
+			System.out.println(key +": " +value);
+				
+			//System.out.println("Key = " + entry.getKey() + 
+                 //   ", Value = " + entry.getValue());
+		}
+		
+	}
+
+}
