a2b5d24d76f5bf96ed6999586acedafbba04c00a
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2024-11-25 10:17:38 +0530

    First non repeatative element

--- src/Arraylistpracticepart2/FirstNonRepeatingElement.java
+++ src/Arraylistpracticepart2/FirstNonRepeatingElement.java
@@ -8,31 +8,20 @@ public class FirstNonRepeatingElement {
 		ar.add(1);
 		ar.add(2);
 		ar.add(3);
-		ar.add(6);
+		ar.add(2);
 		ar.add(1);
+		//ar.add(3);
 		int len1 = ar.size();
 		for(int i=0;i<len1;i++) {
-			int status =0;
-		for(int j=i+1;j<len1;j++) {
-			
-			if(i!=j && ar.get(i)!=ar.get(j)) {
-			
-				status =1;
-			}else {
-				ar.remove(i);
-				ar.remove(j);
-				len1 = ar.size();
-				status =0;
-				break;
+			int j;
+			for(j=0;j<len1;j++) {
+				if(i!=j && ar.get(i)==ar.get(j)) {
+					break;
+				}
 			}
-		}
-		if(status ==1) {
-			System.out.println(ar.get(i));
-			break;
-		}else {
-			
-		}
-		}
+			if(j==len1) {
+				System.out.println(ar.get(i));
+			}
+		}		
 	}
-
 }
