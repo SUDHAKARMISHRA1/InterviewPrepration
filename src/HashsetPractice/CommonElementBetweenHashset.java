00309e2f4f7350d24165428921a1df4b85f82bad
SUDHAKARMISHRA1 <mishrasudhakar5677@gmail.com>
2024-11-29 10:03:20 +0530

    Common element bw hashset

---
+++ src/HashsetPractice/CommonElementBetweenHashset.java
@@ -0,0 +1,23 @@
+package HashsetPractice;
+
+import java.util.HashSet;
+
+public class CommonElementBetweenHashset {
+	
+	public static void main(String[] args) {
+		HashSet<Integer> s = new HashSet<Integer>();
+		s.add(6);
+		s.add(5);
+		s.add(4);	
+		s.add(2);
+		
+		HashSet<Integer> ss = new HashSet<Integer>();
+		ss.add(5);
+		ss.add(2);
+		ss.add(4);	
+		s.retainAll(ss);
+			System.out.println(s);
+		
+	}
+
+}
